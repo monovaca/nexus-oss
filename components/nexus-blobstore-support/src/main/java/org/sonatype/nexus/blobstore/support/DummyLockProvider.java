cf8d962082155ce12d753cf3d2b366ab11629863
Michael Prescott <mprescott@sonatype.com>
2014-04-17 21:16:05 -0400

    Comment.

--- components/nexus-blobstore-support/src/main/java/org/sonatype/nexus/blobstore/support/DummyLockProvider.java
+++ components/nexus-blobstore-support/src/main/java/org/sonatype/nexus/blobstore/support/DummyLockProvider.java
@@ -18,6 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * A null implementation that returns do-nothing locks and never causes threads to wait.
+ *
  * @since 3.0
  */
 public class DummyLockProvider
